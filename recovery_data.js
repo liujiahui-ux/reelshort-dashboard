28002d92f85fbade388e8b8efbfd6ffb3c236d35
liujiahui-ux <liujiahui@crazymaplestudio.com>
2026-05-12 18:34:58 +0800

    Add recovery_data.js with 7 stop records

---
+++ recovery_data.js
@@ -0,0 +1,10 @@
+var RECOVERY_META={update:"2026-05-12 17:00",cooldown:14};
+var STOP_RECORDS=[
+["cr","Married The Mafioso I Saved","2026-05-12"],
+["Mike","Mic Drop Diva","2026-05-12"],
+["wgh","My Double Proxy Marriage to a Billionaire","2026-05-12"],
+["LYX","Married The Mafioso I Saved","2026-05-12"],
+["zhub","Mic Drop Diva","2026-05-12"],
+["FEE","Runaway Single Mom, Safe in His Arms","2026-05-12"],
+["LDH","Married The Mafioso I Saved","2026-05-12"]
+];
